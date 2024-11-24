8a679c671de6c7204bbebce444dce1953f99b9eb
Brian Faust <hello@basecode.sh>
2024-11-24 07:11:04 +0200

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Tests;
+
+use BaseCodeOy\Flash\Flash;
+use BaseCodeOy\PackagePowerPack\TestBench\AbstractPackageTestCase;
+
+/**
+ * @internal
+ */
+abstract class TestCase extends AbstractPackageTestCase
+{
+    protected function getServiceProviderClass(): string
+    {
+        return \BaseCodeOy\Flash\ServiceProvider::class;
+    }
+
+    protected function getPackageAliases($app): array
+    {
+        return ['Flash' => Flash::class];
+    }
+}
