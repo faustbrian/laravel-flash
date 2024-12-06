0a80126fdcd4cc54a685ffe51a386dec92e77163
Brian Faust <hello@basecode.sh>
2024-12-06 17:55:04 +0200

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/Message.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BaseCodeOy\Flash;
+
+use Illuminate\Support\Arr;
+
+final class Message
+{
+    public function __construct(
+        public string $message,
+        public ?string $title = '',
+        public ?string $level = '',
+        public ?array $config = [],
+    ) {}
+
+    public function icon(string $key): array|string
+    {
+        return Arr::get($this->config, 'icon.'.$key);
+    }
+
+    public function class(string $key): string
+    {
+        return Arr::get($this->config, 'classes.'.$key);
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'title' => $this->title,
+            'message' => $this->message,
+            'config' => $this->config,
+            'level' => $this->level,
+        ];
+    }
+}
---
+++ tests/Unit/FlashTest.php
@@ -0,0 +1,113 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Tests\Unit;
+
+use BaseCodeOy\Flash\Flash;
+use BaseCodeOy\Flash\Message;
+
+test('it can set a flash message with a message', function (): void {
+    flash('message');
+
+    expect('message')->toBe(flash()->message);
+});
+
+test('it can set a flash message with a custom title', function (): void {
+    flash('message', 'title');
+
+    expect('message')->toBe(flash()->message);
+    expect('title')->toBe(flash()->title);
+});
+
+test('it can set a flash message with a custom level', function (): void {
+    flash('message', 'title', 'level');
+
+    expect('message')->toBe(flash()->message);
+    expect('level')->toBe(flash()->level);
+});
+
+test('it can set a flash message with a custom config', function (): void {
+    $config = [
+        'icon' => [
+            'name' => 'heroicon-m-x-circle',
+            'class' => 'h-5 w-5 text-red-400',
+            'attributes' => [],
+        ],
+        'classes' => [
+            'background' => 'bg-red-50',
+            'title' => 'text-red-800',
+            'body' => 'text-red-700',
+        ],
+    ];
+
+    flash('message', 'title', 'level', $config);
+
+    expect('message')->toBe(flash()->message);
+    expect($config)->toEqual(flash()->config);
+});
+
+test('it can set a flash message from configured levels', function (): void {
+    flash()->fromConfig();
+
+    flash('message', 'title', 'error');
+
+    expect('message')->toBe(flash()->message);
+    expect([
+        'icon' => [
+            'name' => 'heroicon-m-x-circle',
+            'class' => 'h-5 w-5 text-red-400',
+            'attributes' => [],
+        ],
+        'classes' => [
+            'background' => 'bg-red-50',
+            'title' => 'text-red-800',
+            'body' => 'text-red-700',
+        ],
+    ])->toEqual(flash()->config);
+});
+
+test('the flash function is macroable', function (): void {
+    Flash::macro('info', fn (string $message) => $this->flash(new Message($message)));
+
+    flash()->info('message');
+
+    expect('message')->toBe(flash()->message);
+});
+
+test('multiple methods can be added in one go', function (): void {
+    Flash::levels(config('flash.levels'));
+
+    flash()->warning('warning');
+    expect('warning')->toBe(flash()->message);
+    expect(config('flash.levels.warning'))->toBe(flash()->config);
+
+    flash()->error('error');
+    expect('error')->toBe(flash()->message);
+    expect(config('flash.levels.error'))->toBe(flash()->config);
+});
+
+test('it can get the flash level when the level is registered using the macro', function (): void {
+    Flash::macro('info', fn (string $message) => $this->flashMessage(new Message($message, 'title', 'info')));
+
+    flash()->info('info message');
+
+    expect('info')->toBe(flash()->level);
+});
+
+test('it can get the flash level when levels are registering in one go', function (): void {
+    Flash::levels(config('flash.levels'));
+
+    flash()->error('error');
+
+    expect('error')->toBe(flash()->level);
+});
+
+test('empty flash message returns null', function (): void {
+    expect(flash()->message)->toBeNull();
+});
